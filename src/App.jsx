16fa902774f52346c0c24dfc41338d66b4f1f463
svivek19 <svivek030503@gmail.com>
2024-02-22 19:15:22 +0530

    display data dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import questionData from './questions.json';
+
 const App = () => {
+
+
+  const [currentQues, setCurrentQues] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [timer, setTimer] = useState(100);
+
+  const handleAnswerClick = () => {
+    
+  }
+
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-slate-800'>
-      <div className='bg-white p-5 rounded-md shadow-black shadow-md'>
-
-          <div className='text-center hidden'>
+      <div className='bg-white p-5 w-[80%] md:w-[50%] rounded-md shadow-black shadow-md'>
+        {showScore ? (
+          <div className='text-center'>
             <h2 className="font-semibold text-xl">Your Score: 3/3</h2>
             <button className="bg-red-500 my-3 text-white font-bold py-2 px-4 rounded-full">
               Restart
             </button>
           </div>
+        ) : (
+          <div className='text-center'>
+            <h2 className="font-bold text-3xl text-blue-500">Question {currentQues + 1}</h2>
+            <p className='my-4 text-xl font-medium'>{questionData[currentQues].question}</p>
+            <div className="space-x-4 my-6">
+              {questionData[currentQues].options.map((option, index) => (
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full" key={index} onClick={() => handleAnswerClick(option)}>{option}</button>
+              ))}
 
-        <div className='text-center'>
-          <h2 className="font-bold text-2xl text-blue-500">Question 1</h2>
-          <p className='my-3 font-medium'>Sample question</p>
-          <div className="space-x-4 my-3">
-            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full">
-              Option 1
-            </button>
-            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full">
-              Option 2
-            </button>
-            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full">
-              Option 3
-            </button>
-            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full">
-              Option 4
-            </button>
+            </div>
+            <div className='font-semibold'>Time Left: <span className='font-bold'>{timer}s</span></div>
           </div>
-          <div className='font-semibold'>Time Left: <span className='font-bold'>5s</span></div>
-        </div>
+        )}
+
+
+
       </div>
     </div>
 
